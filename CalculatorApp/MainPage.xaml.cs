2bfb22c1dce5940547ceaebcb0370585f09441b5
clandy07 <127475702+clandy07@users.noreply.github.com>
2024-03-06 02:32:06 +0800

    First (and probably only) commit!
    -Logic and function are pretty much done.
    -Did our best to stay true to given design
    
    (Note: The project's "bin" and "obj" folders were deleted to ensure that the project could be uploaded on this platform.)

---
+++ CalculatorApp/MainPage.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Maui.Controls;
+
+namespace Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        string currentNumber = "0";
+        string selectedOperator;
+        double result = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnClear(object sender, EventArgs e)
+        {
+            result = 0;
+            currentNumber = "0";
+            resultText.Text = currentNumber;
+        }
+
+        void OnDelete(object sender, EventArgs e)
+        {
+            if (currentNumber.Length > 1)
+            {
+                currentNumber = currentNumber.Remove(currentNumber.Length - 1);
+            }
+            else
+            {
+                currentNumber = "0";
+            }
+            resultText.Text = currentNumber;
+        }
+
+        void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            string pressed = button.Text;
+            
+            if (pressed == "." && currentNumber.Contains("."))
+            {
+                return;
+            }
+
+            if (currentNumber == "0")
+            {
+                currentNumber = pressed;
+            }
+            else
+            {
+                currentNumber += pressed;
+            }
+            resultText.Text = currentNumber;
+        }
+
+
+        void OnSelectOperator(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            selectedOperator = button.Text;
+
+            if (double.TryParse(currentNumber, out double parsedNumber))
+            {
+                result = parsedNumber;
+                currentNumber = "0";
+                resultText.Text = currentNumber;
+            }
+        }
+
+        void OnCalculate(object sender, EventArgs e)
+        {
+            if (double.TryParse(currentNumber, out double parsedNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case "+":
+                        result += parsedNumber;
+                        break;
+                    case "-":
+                        result -= parsedNumber;
+                        break;
+                    case "×":
+                        result *= parsedNumber;
+                        break;
+                    case "/":
+                        if (parsedNumber != 0)
+                        {
+                            result /= parsedNumber;
+                        }
+                        else
+                        {
+                            resultText.Text = "Error";
+                            return;
+                        }
+                        break;
+                }
+                currentNumber = result.ToString();
+                resultText.Text = currentNumber;
+            }
+        }
+    }
+}
